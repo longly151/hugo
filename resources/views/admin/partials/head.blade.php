b39616c828c041f0f368990576f32d4daf6090bd
Le Viet Long <vilagondk@gmail.com>
2019-03-02 23:21:13 +0700

    update

--- resources/views/admin/partials/head.blade.php
+++ resources/views/admin/partials/head.blade.php
@@ -1,5 +1,6 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 <!-- Tell the browser to be responsive to screen width -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
@@ -8,5 +9,6 @@
 <!-- Favicon icon -->
 <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
 <title>Admin Press Admin Template - The Ultimate Bootstrap 4 Admin Template</title>
+
 <!-- Bootstrap Core CSS -->
 @include('admin.partials.stylesheets')
