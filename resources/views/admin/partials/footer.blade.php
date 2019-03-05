d85e6969031428ce0051037b908bac1daafa345d
Le Viet Long <vilagondk@gmail.com>
2019-03-05 22:20:52 +0700

    update

--- resources/views/admin/partials/footer.blade.php
+++ resources/views/admin/partials/footer.blade.php
@@ -1,3 +1,4 @@
 <footer class="footer">
-  © 2017 Admin Press Admin by themedesigner.in
+  © Copyright by <a href="https://www.facebook.com/vietlong.le.503" target="_blank">Vilagon</a>
+  Designed by Admin Press Admin by themedesigner.in
 </footer>
